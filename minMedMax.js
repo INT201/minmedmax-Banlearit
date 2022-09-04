f70866dc5d5f6dc164f67c5846ba1bb5f7b2e352
Banlearit Siriboon <banlearit.ares@mail.kmutt.ac.th>
2022-09-05 00:17:58 +0700

    fix: minMedMax succeed

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,20 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+    let num = [n1, n2, n3];
+    let sum;
+    for (let i = 0; i < num.length; i++) {
+        for (let j = i + 1; j < num.length; j++) {
+            if (num[i] > num[j]) {
+                sum = num[i];
+                num[i] = num[j];
+                num[j] = sum;
+            }
+        }
+    }
+    return {min: num[0], mid: num[1], max: num[2]}
 }
+console.log(minMedMax(10,0,20));
+
+
 module.exports = minMedMax
